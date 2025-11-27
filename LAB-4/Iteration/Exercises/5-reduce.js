5ddcbb6980b136778ab684ac2ce9a396e2b4ec94
ranenneyoulikemore <91974716+ranenneyoulikemore@users.noreply.github.com>
2025-11-27 14:10:34 +0200

    Delete LAB-4/Iteration directory

--- LAB-4/Iteration/Exercises/5-reduce.js
+++
@@ -1,6 +0,0 @@
-function sum(...args){
-
-    return args.reduce((total, num) => total + num, 0)
-}
-const a = sum(12, 90, 64);
-console.log(a)
--- LAB-4/Iteration/Exercises/6-matrix.js
+++
@@ -1,5 +0,0 @@
-const matrix = [[5, 9, 14], [2,5,7], [0,1,4]];
-
-const m = Math.max(...matrix.flat());
-
-console.log(m)
